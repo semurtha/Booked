f5629d82944d48ce5f7308cb8f7111f025e25531
semurtha <57686009+semurtha@users.noreply.github.com>
2021-11-08 14:00:15 -0500

    Created a Review class to store data more easily  - TODO does not reflect things to do only in this file

---
+++ app/src/main/java/com/semurtha/booked/Review.java
@@ -0,0 +1,97 @@
+package com.semurtha.booked;
+
+import java.util.Date;
+
+public class Review {
+    private String bookTitle, reviewTitle, reviewContent;
+    private float rating;
+    private boolean favorited;
+    private String reviewedBy;
+    private Date reviewDate;
+
+    public Review() {
+    }
+
+    public Review (String bookTitle, float rating, boolean favorited, String reviewTitle, String reviewContent, String reviewedBy, Date reviewDate) {
+        this.bookTitle = bookTitle;
+        this.rating = rating;
+        this.favorited = favorited;
+        this.reviewTitle = reviewTitle;
+        this.reviewContent = reviewContent;
+        this.reviewedBy = reviewedBy;
+        this.reviewDate = reviewDate;
+    }
+
+    @Override
+    public String toString() {
+        return  "\nbookTitle='" + bookTitle + '\'' + '\n' +
+                "reviewTitle='" + reviewTitle + '\'' + '\n' +
+                "reviewContent='" + reviewContent + '\'' + '\n' +
+                "rating=" + rating + '\n' +
+                "favorited=" + favorited + '\n' +
+                "reviewedBy='" + reviewedBy + '\'' + '\n' +
+                "reviewDate=" + reviewDate + '\n';
+    }
+
+    /*
+    TODO : Add validation for submitting reviews - nothing left blank
+    TODO : Swap from list view to recyclerview w/ cards
+    TODO : Handle adding a book to users' favorites
+    TODO : Swap from default appbar to toolbar
+    */
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getReviewTitle() {
+        return reviewTitle;
+    }
+
+    public void setReviewTitle(String reviewTitle) {
+        this.reviewTitle = reviewTitle;
+    }
+
+    public String getReviewContent() {
+        return reviewContent;
+    }
+
+    public void setReviewContent(String reviewContent) {
+        this.reviewContent = reviewContent;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public String getReviewedBy() {
+        return reviewedBy;
+    }
+
+    public void setReviewedBy(String reviewedBy) {
+        this.reviewedBy = reviewedBy;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+}
